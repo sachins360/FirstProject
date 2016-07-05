044890f529bf0b8e3281c6de26b6cd4c650228a1
Xamarin77 <xamarin77@Xamarin77Macmini.local>
2016-07-05 12:42:17 +0530

    Add a ready line.

--- TestProjectConsole/TestProjectConsole/Program.cs
+++ TestProjectConsole/TestProjectConsole/Program.cs
@@ -6,7 +6,10 @@ namespace TestProjectConsole
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			
+Console.WriteLine("Hello World!");
+			Console.ReadKey();
+			
 		}
 	}
 }
