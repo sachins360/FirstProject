91778b46f76a64f1a80b52da4f5f193655d996b5
Xamarin77 <xamarin77@Xamarin77Macmini.local>
2016-07-05 12:49:33 +0530

    test1

--- TestProjectConsole/TestProjectConsole/Program.cs
+++ TestProjectConsole/TestProjectConsole/Program.cs
@@ -7,8 +7,9 @@ namespace TestProjectConsole
 		public static void Main(string[] args)
 		{
 			
-Console.WriteLine("Hello World!");
-			Console.ReadKey();
+             Console.WriteLine("Hello World!");
+			
+             Console.ReadKey();
 			
 		}
 	}
